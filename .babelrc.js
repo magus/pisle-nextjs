509c1dca8aa968d211a76b3198d37d40fcd7a4e4
Noah Jorgensen <noah@iamnoah.com>
2019-09-08 13:55:17 -0700

    Update .babelrc.js

--- .babelrc.js
+++ .babelrc.js
@@ -1,27 +1,19 @@
 const env = process.env.BABEL_ENV || process.env.NODE_ENV;
 
 const plugins = [
-  ["babel-plugin-root-import"],
-  ["babel-plugin-transform-flow-strip-types"],
+  ['babel-plugin-root-import'],
+  ['babel-plugin-transform-flow-strip-types'],
   [
-    "babel-plugin-styled-components",
+    'babel-plugin-styled-components',
     {
-      "ssr": true,
-      "displayName": true,
-      "preprocess": false
-    }
-  ]
+      ssr: true,
+      displayName: true,
+      preprocess: false,
+    },
+  ],
 ];
 
-if (env === 'production') {
-  plugins.push(
-    ["react-intl", {
-      "messagesDir": "lang/.messages/"
-    }]
-  );
-}
-
 module.exports = {
-  "presets": [["next/babel"]],
-  "plugins": plugins,
+  presets: [['next/babel']],
+  plugins: plugins,
 };
